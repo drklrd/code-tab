22f9ebb32654e937f6a4aa421b89760718033e19
drklrd <srvbhattarai@gmail.com>
2017-11-29 00:05:48 +0545

    add material select

--- js/newtab.js
+++ js/newtab.js
@@ -8,11 +8,17 @@
  	fontSize: "15pt"
  });
 
- document.getElementById('selector').addEventListener('change', (evt) => {
- 	 JavaScriptMode = ace.require(`ace/mode/${evt.target.value}`).Mode;
- 	 editor.session.setMode(new JavaScriptMode());
- });
+
+ var classname = document.getElementsByClassName("selector");
+ var myFunction = function() {
+ 	var attribute = this.getAttribute("data-myattribute");
+ 	JavaScriptMode = ace.require(`ace/mode/${attribute}`).Mode;
+ 	editor.session.setMode(new JavaScriptMode());
+ };
+
+ for (var i = 0; i < classname.length; i++) {
+ 	classname[i].addEventListener('click', myFunction, false);
+ }
 
 
-
-
+ $.material.init();
