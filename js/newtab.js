4b9d392237f3ee4f85c615e3e78bc08830201097
drklrd <srvbhattarai@gmail.com>
2017-11-28 22:07:13 +0545

    basic selection functionaloty

--- js/newtab.js
+++ js/newtab.js
@@ -1,2 +1,19 @@
  var editor = ace.edit("editor");
  editor.setTheme("ace/theme/monokai");
+
+ var JavaScriptMode = ace.require("ace/mode/javascript").Mode;
+ editor.session.setMode(new JavaScriptMode());
+
+ editor.setOptions({
+ 	fontSize: "15pt"
+ });
+
+
+ function changeLanguage(){
+ 	alert(8)
+ }
+
+ document.getElementById('selector').addEventListener('change', () => {
+ 	alert(88)
+
+ });
