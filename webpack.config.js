914bd8dddc443dc875626e8e2f14149738235ffe
drklrd <srvbhattarai@gmail.com>
2017-11-29 19:56:23 +0545

    porting code to react

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+	entry : [
+		'./src/index.js'
+	],
+
+	output : {
+		path :  __dirname + '/dist',
+		filename : "bundle.js"
+	},
+	module : {
+
+		loaders : [
+			{
+				test : /\.js$/, 
+				exclude : /node_modules|bower_components/, 
+				loader : "babel-loader",
+				query : {
+					presets: ['react']
+				}
+			}
+
+		]
+	}
+
+}
