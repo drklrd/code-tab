e54d89ef27257e7e9a6871fa4cf9a6f4520f3609
drklrd <srvbhattarai@gmail.com>
2017-11-29 19:38:49 +0545

    few changes

--- js/newtab.js
+++ js/newtab.js
@@ -1,5 +1,5 @@
  var editor = ace.edit("editor");
- editor.setTheme("ace/theme/dracula");
+ editor.setTheme("ace/theme/monokai");
 
  var JavaScriptMode = ace.require("ace/mode/javascript").Mode;
  editor.session.setMode(new JavaScriptMode());
@@ -8,17 +8,22 @@
  	fontSize: "15pt"
  });
 
+ document.getElementById('s1').addEventListener('change', (evt) => {
+ 	 JavaScriptMode = ace.require(`ace/mode/${evt.target.value}`).Mode;
+ 	 editor.session.setMode(new JavaScriptMode());
+ });
 
- var classname = document.getElementsByClassName("selector");
- var myFunction = function() {
- 	var attribute = this.getAttribute("data-myattribute");
- 	JavaScriptMode = ace.require(`ace/mode/${attribute}`).Mode;
- 	editor.session.setMode(new JavaScriptMode());
- };
 
- for (var i = 0; i < classname.length; i++) {
- 	classname[i].addEventListener('click', myFunction, false);
- }
+ // var classname = document.getElementsByClassName("selector");
+ // var myFunction = function() {
+ // 	var attribute = this.getAttribute("data-myattribute");
+ // 	JavaScriptMode = ace.require(`ace/mode/${attribute}`).Mode;
+ // 	editor.session.setMode(new JavaScriptMode());
+ // };
+
+ // for (var i = 0; i < classname.length; i++) {
+ // 	classname[i].addEventListener('click', myFunction, false);
+ // }
 
 
  $.material.init();
