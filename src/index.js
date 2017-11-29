4108772ab38cefe7c1cbf716dfe09a52db28312e
drklrd <srvbhattarai@gmail.com>
2017-11-29 20:12:27 +0545

    add react-ace

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import brace from 'brace';
+import AceEditor from 'react-ace';
+
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
 
 class App extends React.Component{
 
 	render(){
 		return(
-			<div>
-		        <h1>HELLO</h1>
+			<div className="container-fluid">
+		       <div className="row">
+				   <div className="col-xs-12">
+						<AceEditor
+							mode="javascript"
+							theme="monokai"
+							width="100%"
+							height="100vh"
+							name="UNIQUE_ID_OF_DIV"
+							editorProps={{$blockScrolling: true}}
+						/>
+				   </div>
+			   </div>
 			</div>
 
 		)
