b0149882137ad3a1327d8d71c55c282d1e33cbeb
drklrd <srvbhattarai@gmail.com>
2017-11-30 21:11:09 +0545

    fix issue with mdoe update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SelectComponent from './select';
 import { split } from 'react-ace';
+import Button from 'material-ui/Button';
+import Icon from 'material-ui/Icon';
 
 import './acemodes';
 
@@ -11,14 +13,22 @@ class App extends React.Component{
 	constructor(props){
         super(props);
 		this.state = {
-			mode : 'python'
+			mode : 'python',
+			fontSize : 20
 		};
         this.updateMode = this.updateMode.bind(this);
+        this.alterFontSize = this.alterFontSize.bind(this);
     }
 
 	updateMode(mode){
-		this.setState({
-			mode
+        this.refs['editor'].editor.session.setMode(`ace/mode/${mode}`);
+	}
+
+	alterFontSize(upDown){
+		let editorInstance = this.refs['editor'].editor;
+		let updatedSize = upDown == 'up' ? editorInstance.getFontSize() + 5 : editorInstance.getFontSize() - 5;
+        editorInstance.setOptions({
+            fontSize: updatedSize
 		});
 	}
 
@@ -28,9 +38,10 @@ class App extends React.Component{
 			<div className="row no-margin">
 				<div className="col-xs-12">
 					<AceEditor
+						ref={"editor"}
 						splits={2}
     					orientation="beside"
-						fontSize="20px"
+						fontSize={this.state.fontSize}
 						mode={this.state.mode}
 						theme="cobalt"
 						width="100%"
@@ -41,6 +52,23 @@ class App extends React.Component{
 				</div>
 				<div className="floating font-large">
 					<SelectComponent mode={this.state.mode} updateMode={this.updateMode}/>
+				</div>
+				<div className="floating-buttons font-large">
+					<div className="row">
+						<div className="col-xs-6">
+							<Button raised onClick={()=>{this.alterFontSize('up')}}>
+								Size &nbsp;
+								<Icon>add_circle</Icon>
+							</Button>
+						</div>
+						<div className="col-xs-6">
+							<Button raised onClick={()=>{this.alterFontSize('down')}}>
+								Size &nbsp;
+								<Icon>remove_circle</Icon>
+							</Button>
+						</div>
+					</div>
+
 				</div>
 				<div className="code-tab">
 					<span> {codeTab} </span>
